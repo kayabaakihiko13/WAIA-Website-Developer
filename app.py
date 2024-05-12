38ed8f0f2380be7ac390f0bedc901aedc73dda05
kayabaakihiko13 <iqbalramad75@gmail.com>
2024-05-12 23:01:32 +0700

    feat: update make UI register with database

--- app.py
+++ app.py
@@ -4,10 +4,12 @@ from dotenv import load_dotenv
 import os
 from src.views import bp as index_bp
 from src import db
+from config import UPLOAD_PATH
 def create_app() -> Flask:
     load_dotenv()
     app = Flask(__name__)
     app.config["SQLALCHEMY_DATABASE_URI"]= os.environ.get("DB_URL")
+    app.config["UPLOAD_FOLDER"] =UPLOAD_PATH 
     db.init_app(app)
     with app.app_context():
         db.create_all()
